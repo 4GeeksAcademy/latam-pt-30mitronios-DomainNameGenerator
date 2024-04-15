565550e45ab2c58d44923c7af059e36494271bf1
Miguel Angel <160284274+Mitronios@users.noreply.github.com>
2024-04-15 00:12:34 +0000

    Domain name first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = `<ul>${generateDomain
+      .map(domainName => `<li>${domainName}</li>`)
+      .join(" ")}</ul>`;
+  });
 };
+let pronouns = ["the", "our", "your"];
+let adjectives = ["black", "big", "awesome", "cute", "superb"];
+let nouns = ["cat", "racoon", "blog", "website", "domain"];
+let tlds = [".com", ".es", ".us", ".org", ".bo", ".nz", ".ca"];
+
+let generateDomain = [];
+
+for (let pronoun of pronouns) {
+  for (let adjective of adjectives) {
+    for (let noun of nouns) {
+      for (let tld of tlds) {
+        generateDomain.push(`${pronoun}${adjective}${noun}${tld}`);
+      }
+    }
+  }
+}
